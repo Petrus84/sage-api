7b7ba35708b33645b066f20e6f281cc9c83ca485
PETRUCIO NOVAES DE BARROS <petrucionovaes@gmail.com>
2025-10-30 15:59:12 -0300

    "security: sanitize env example file"

--- env_example.sh
+++ env_example.sh
@@ -2,7 +2,7 @@
 # Copie para .env e configure com suas credenciais
 
 # MongoDB Atlas
-MONGODB_URI=mongodb+srv://petrucionovaes_db_user:EmjbUQvwKzFqP2l2@sage-nfm-cluster.e3fpwtf.mongodb.net/sage-nfm?appName=sage-nfm-cluster
+MONGODB_URI=mongodb+srv://SEU_USUARIO:SUA_SENHA@seu-cluster.mongodb.net/sage-nfm?appName=sage-nfm-cluster
 
 # JWT Secret (use um hash forte)
 JWT_SECRET=seu-secret-super-seguro-aqui-min-32-caracteres
